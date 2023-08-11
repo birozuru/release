47313e1dfdc7b8bb274a445edf8028cea57b8a02
birozuru <birozuru@yahoo.com>
2023-08-11 07:11:45 +0100

    release test 1

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 // program for a simple calculatorr
 let result;
 
@@ -10,28 +8,28 @@ const operator = prompt('Enter operator ( either +, -, * or / ): ');
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
-switch(operator) {
-    case '+':
-         result = number1 + number2;
-        console.log(`${number1} + ${number2} = ${result}`);
-        break;
+switch (operator) {
+  case '+':
+    result = number1 + number2;
+    console.log(`${number1} + ${number2} = ${result}`);
+    break;
 
-    case '-':
-         result = number1 - number2;
-        console.log(`${number1} - ${number2} = ${result}`);
-        break;
+  case '-':
+    result = number1 - number2;
+    console.log(`${number1} - ${number2} = ${result}`);
+    break;
 
-    case '*':
-         result = number1 * number2;
-        console.log(`${number1} * ${number2} = ${result}`);
-        break;
+  case '*':
+    result = number1 * number2;
+    console.log(`${number1} * ${number2} = ${result}`);
+    break;
 
-    case '/':
-         result = number1 / number2;
-        console.log(`${number1} / ${number2} = ${result}`);
-        break;
+  case '/':
+    result = number1 / number2;
+    console.log(`${number1} / ${number2} = ${result}`);
+    break;
 
-    default:
-        console.log('Invalid operator');
-        break;
+  default:
+    console.log('Invalid operator');
+    break;
 }
